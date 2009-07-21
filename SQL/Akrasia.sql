f6f63e8625e50d82b82b20a605fb0a061b1cb8a4
asw12 <asw12@berkeley.edu>
2009-07-21 16:47:17 -0700

    Add settings for generating .sqls through Jdev Note: add *.schema and *.offlinedb to git exclude after the structure is tested
    Signed-off-by: asw12 <asw12@berkeley.edu>

--- SQL/Akrasia.sql
+++ SQL/Akrasia.sql
@@ -1,43 +1,24 @@
-/*
-SQLyog Community Edition- MySQL GUI v7.15 
-MySQL - 5.1.30-community : Database - akrasia
-*********************************************************************
-*/
+CREATE TABLE dungeonlevel 
+( 
+  `entry` INT(10) UNSIGNED NOT NULL 
+, `environment` INT(10) NOT NULL 
+, CONSTRAINT PRIMARY KEY ( `entry` ) );
 
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-/*Table structure for table `creaturetemplate` */
-
-CREATE TABLE IF NOT EXISTS `creaturetemplate` (
-  `entry` int(10) unsigned NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `char` varchar(9) NOT NULL DEFAULT 'M',
-  `levelmin` int(10) unsigned NOT NULL DEFAULT '1',
-  `levelmax` int(10) unsigned DEFAULT '1',
-  `size` int(10) unsigned NOT NULL DEFAULT '1',
-  `type` int(10) unsigned NOT NULL DEFAULT '1' COMMENT 'Should be an enum',
-  `hitdie` int(10) unsigned NOT NULL DEFAULT '2',
-  `armorclass` int(10) unsigned NOT NULL DEFAULT '0',
-  `evade` varchar(255) NOT NULL DEFAULT '0',
-  `accuracy` int(10) unsigned NOT NULL DEFAULT '1',
-  `speed` int(10) unsigned NOT NULL DEFAULT '1',
-  `descriptionshort` text,
-  `descriptionlong` text,
-  `AI` int(11) DEFAULT NULL,
-  PRIMARY KEY (`entry`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Table structure for table `dungeonlevel` */
-
-CREATE TABLE IF NOT EXISTS `dungeonlevel` (
-  `entry` int(5) unsigned NOT NULL,
-  `environment` enum('dungeon','city','forest','plains','mountains','water') NOT NULL,
-  PRIMARY KEY (`entry`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+CREATE TABLE creaturetemplate 
+( 
+  `entry` INT(10) UNSIGNED NOT NULL 
+, `name` VARCHAR(255) NULL 
+, `char` VARCHAR(9) NOT NULL DEFAULT 'M' 
+, `levelmin` INT(10) UNSIGNED NOT NULL DEFAULT 1 
+, `levelmax` INT(10) UNSIGNED NULL DEFAULT 1 
+, `size` INT(10) UNSIGNED NOT NULL DEFAULT 1 
+, `type` INT(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT 'Should be an enum' 
+, `hitdie` INT(10) UNSIGNED NOT NULL DEFAULT 2 
+, `armorclass` INT(10) UNSIGNED NOT NULL DEFAULT 0 
+, `evade` VARCHAR(255) NOT NULL DEFAULT '0' 
+, `accuracy` INT(10) UNSIGNED NOT NULL DEFAULT 1 
+, `speed` INT(10) UNSIGNED NOT NULL DEFAULT 1 
+, `descriptionshort` TEXT NULL 
+, `descriptionlong` TEXT NULL 
+, `AI` INT(10) NULL 
+, CONSTRAINT PRIMARY KEY ( `entry` ) );
